9210c09ffe32b5562c68b21a12bccf20a8a1d455
Kiera Allen <kieraaallen@gmail.com>
2020-03-18 14:03:16 -0600

    Add restaurant_test.rb file

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -11,44 +11,45 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
   end
 
   def test_it_has_dishes
-    skip
+    # skip
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
   end
+end
 
   #Iteration 2 Tests:
-  def test_it_has_closing_time
-    skip
-    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
-
-    assert_equal '18:00', restaurant1.closing_time(8)
-    assert_equal '23:00', restaurant2.closing_time(7)
-  end
-
-  def test_it_can_add_dishes
-    skip
-    restaurant = Restaurant.new('16:00', 'Il Poggio')
-
-    restaurant.add_dish('Burrata')
-    restaurant.add_dish('Pizzetta')
-    restaurant.add_dish('Ravioli')
-
-    assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
-  end
-end
+#   def test_it_has_closing_time
+#     skip
+#     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+#     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+#
+#     assert_equal '18:00', restaurant1.closing_time(8)
+#     assert_equal '23:00', restaurant2.closing_time(7)
+#   end
+#
+#   def test_it_can_add_dishes
+#     skip
+#     restaurant = Restaurant.new('16:00', 'Il Poggio')
+#
+#     restaurant.add_dish('Burrata')
+#     restaurant.add_dish('Pizzetta')
+#     restaurant.add_dish('Ravioli')
+#
+#     assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
+#   end
+# end
