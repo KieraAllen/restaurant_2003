fe1dddbf522519c8e5d24de5bb491b1188ec22ec
Kiera Allen <kieraaallen@gmail.com>
2020-03-18 14:02:10 -0600

    Add restaurant.rb and runner.rb files

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,20 @@
+class Restaurant
+  def initialize(opening_time_parameter, name_parameter)
+    @opening_time = opening_time_parameter
+    @name = name_parameter
+    @dishes = []
+  end
 
+  def name
+    @name
+  end
+
+  def opening_time
+    @opening_time
+  end
+
+  def dishes
+    @dishes
+  end
+  
+end
---
+++ lib/runner.rb
@@ -0,0 +1,5 @@
+require './restaurant'
+
+restaurant = Restaurant.new("10:00", "Fuel Cafe")
+
+require 'pry'; binding.pry
